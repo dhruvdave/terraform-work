0b4a81cc51f04ad9fa20007c15d3ebd323869634
dhruvdave <dhruvdave.ce@gmail.com>
2023-07-28 10:37:09 +0530

    Update variable.tf
    Update the variable file - Remove Access key and Secret Key

--- First_Terraform/variable.tf
+++ First_Terraform/variable.tf
@@ -1,18 +1,7 @@
 # variable.tf 
 
 # No default value
-variable "access_key" {
-  type        = string
-  description = "access key for AWS Account"
-  default     = "AKIAUICWUDPZTVK2LC6W"
-}
 
-variable "secret_key" {
-  type        = string
-  description = "Secret key for AWS Account"
-  default     = "OmV6sJD2my6RroPixh/BtjGFaHRqZ8jGJzgUfeTI"
-
-}
 
 variable "instance_type" {
   type        = string
